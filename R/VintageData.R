ec8cc75de8ed3a2a4e75dd634efec30e9407c221
Tomáš Greif <tgr>
2013-11-01 15:08:35 +0100

    Added sample pre-calculated data to be used with plotting aggregation functions or with ptvintshiny

---
+++ R/VintageData.R
@@ -0,0 +1,15 @@
+#' Vintage data
+#' 
+#'  This dataset contains sample pre-calculated vintage data to play with. This dataset can be directly used in functions
+#'  \code{PlotVintageData} and \code{AggregateVintageData} or used with Shiny application \code{pgvintshiny}
+#' 
+#' \itemize{
+#'   \item Vintage Data
+#' }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A sample dataset for demonstration purposes.
+#' @name VintageData
+
+NULL
